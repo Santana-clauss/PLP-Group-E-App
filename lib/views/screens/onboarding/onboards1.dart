590d7c785620bb5ad026333f106bc2f01e5d7001
santana clauss <jepchumbasantana@gmail.com>
2024-06-04 23:00:20 +0300

    updates

--- lib/views/screens/onboarding/onboards1.dart
+++ lib/views/screens/onboarding/onboards1.dart
@@ -1,39 +1,53 @@
-// // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+import 'package:flutter/material.dart';
 
-// import 'package:flutter/material.dart';
-// import 'package:shopeasy/views/customtext.dart';
 
-// final controller=PageController();
-// class OnboardingScreen extends StatelessWidget {
-//   const OnboardingScreen({super.key});
+class FirstPage extends StatelessWidget {
+  final String heading;
+  final String text;
+  final String image;
+  const FirstPage(
+      {super.key,
+      required this.heading,
+      required this.text,
+      required this.image});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Container(
-//         padding: EdgeInsets.only(bottom: 80),
- 
-//         child: PageView(
-//           controller: controller,
-//           children: [
-//             Container(),
-//             Container(),
-//             Container()
-//           ],
-//         ),
-//       ),
-//       bottomSheet: Container(
-//         padding: EdgeInsets.symmetric(horizontal:80),
-//         height: 80,
-//         child: Row(
-//           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//            children: [
-//             customText(label: ""),
-//             smoothpa
-//             customText(label: "")
-//            ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 120),
+      child: SingleChildScrollView(
+        child: Column(
+          // crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset(
+              image,
+              height: 356,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Column(
+              children: [
+                CustomText(
+                  text: heading,
+                  textcolor: secondarycolor,
+                  fontWeight: FontWeight.bold,
+                  fontsize: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: CustomText(
+                    text: text,
+                    fontWeight: FontWeight.w500,
+                    fontsize: 16,
+                    textcolor: primarycolor,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
